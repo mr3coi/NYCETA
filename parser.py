6c75c7e5d0a36364f409e2438d549e741d2eb807
nilesc@github.com <nnc2117@columbia.edu>
2019-10-24 21:19:15 -0400

    [Changes] parser to actually read files

--- parser.py
+++ parser.py
@@ -1,15 +1,31 @@
 """ Contains code to read CSV files containing taxi data """
 import sys
 from glob import glob
+import csv
 
 def parse_files(file_regex):
     """Parses the context of files matching file_regex
 
-    :file_regex: TODO
-    :returns: TODO
+    :file_regex: a regular expression matching the files whose contents we
+        want to parse
+    :returns: A list of lists, each inner list containing a csv row
 
     """
-    print(glob(file_regex))
+    matching_files = glob(file_regex)
+
+    all_rows = []
+
+    for current_file in matching_files:
+        with open(current_file) as opened_file:
+            file_reader = csv.reader(opened_file)
+
+            # Skip the header row
+            next(file_reader)
+
+            for row in file_reader:
+                all_rows.append(row)
+
+    return all_rows
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
