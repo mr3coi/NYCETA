a10c8ceb6bc8531cc74bb8ee93e3bda57c8a1277
nilesc@github.com <nnc2117@columbia.edu>
2019-10-26 18:04:25 -0400

    [Changes] to use pandas

--- parser.py
+++ parser.py
@@ -1,43 +1,23 @@
 """ Contains code to read CSV files containing taxi data """
 import sys
 from glob import glob
-import csv
+import pandas as pd
 
-def parse_files(file_regex, total_rows=None):
+
+def parse_files(file_regex):
     """Parses the context of files matching file_regex
 
     :file_regex: a regular expression matching the files whose contents we
         want to parse
-    :total_rows: if not None, the total number of rows to return. Will read this
-        many and return early.
-    :returns: A list of lists, each inner list containing a csv row
+    :returns: A pandas dataframe containing every row in the specified files
 
     """
     matching_files = glob(file_regex)
 
-    all_rows = []
+    all_data_frames = (pd.read_csv(f, header=0) for f in matching_files)
+    merged_data = pd.concat((all_data_frames))
 
-    rows_read = 0
-
-    for current_file in matching_files:
-        with open(current_file) as opened_file:
-            file_reader = csv.reader(opened_file)
-
-            # Skip the header row
-            next(file_reader)
-
-            for row in file_reader:
-                all_rows.append(row)
-                rows_read += 1
-
-                if total_rows and rows_read == total_rows:
-                    break
-
-            if total_rows and rows_read == total_rows:
-                break
-
-    print(len(all_rows))
-    return all_rows
+    return merged_data
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
