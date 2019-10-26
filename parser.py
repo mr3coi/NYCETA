e161a6f0b05a3d20ac97868dd5ad2138edca24db
nilesc@github.com <nnc2117@columbia.edu>
2019-10-26 18:26:12 -0400

    [Adds] the ability to write the parsed data to a db

--- parser.py
+++ parser.py
@@ -2,6 +2,7 @@
 import sys
 from glob import glob
 import pandas as pd
+import sqlite3
 
 DATE_COLUMNS = [
         'tpep_pickup_datetime',
@@ -26,7 +27,46 @@ def parse_files(file_regex):
 
     return merged_data
 
+
+def write_to_db(dataframe, db_conn, table_name):
+    """Writes the given dataframe to a database table. If the table
+    already exists, it will overwrite its contents.
+
+    :dataframe: The dataframe to write to a database table
+    :db_conn: The database connection object
+    :table_name: The name of the table to which we will write
+
+    """
+    dataframe.to_sql(table_name, db_conn, if_exists='replace')
+
+
+def parse_files_and_write_to_db(file_regex, db_conn, table_name):
+    """
+    Parses the given files and writes them to a table in a database.
+
+    :file_regex: a regular expression matching the files whose contents we
+        want to parse
+    :db_conn: The database connection object
+    :table_name: The name of the table to which we will write
+
+    """
+    all_data = parse_files(file_regex)
+    write_to_db(all_data, db_conn, table_name)
+
+
+def main(file_regex):
+    """
+    Calls parse_files_and_write_to_db.
+
+    :file_regex: The regex containing the files to parse.
+
+    """
+    table_name = 'rides'
+    db_conn = sqlite3.connect('rides.db')
+    parse_files_and_write_to_db(file_regex, db_conn, table_name)
+
+
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         sys.exit('Please supply a path to files to parse')
-    parse_files(sys.argv[1])
+    main(sys.argv[1])
