ff8b32d4a835e3950cf2a8bdc53db04c666f4da2
Amrit Singhal <amritsinghal97@gmail.com>
2019-12-02 01:23:55 -0500

    added training code

--- boro_model.py
+++ boro_model.py
@@ -1,20 +1,19 @@
 import numpy as np 
 import tensorflow as tf 
-from tf.keras.layers import Dense
 
 class BoroModel(tf.keras.Model):
 
 	def __init__(self, num_neurons_in_layers=[100,50]):
 		super(BoroModel, self).__init__()
-		self.dense1 = Dense(num_neurons_in_layers[0], 
+		self.dense1 = tf.keras.layers.Dense(num_neurons_in_layers[0], 
 				activation=tf.nn.relu)
-		self.dense2 = Dense(num_neurons_in_layers[1],
+		self.dense2 = tf.keras.layers.Dense(num_neurons_in_layers[1],
 				activation=tf.nn.relu)
-		self.dense3 = Dense(1, activation=tf.nn.relu)
+		self.dense3 = tf.keras.layers.Dense(1, activation=tf.nn.relu)
 
 
 	def call(self, inputs):
 		x = self.dense1(inputs)
 		x = self.dense2(x)
-		y = self.dense3(y)
+		y = self.dense3(x)
 		return y
---
+++ train_boro_model.py
@@ -0,0 +1,103 @@
+import os
+import datetime
+import argparse
+import numpy as np
+from boro_model import BoroModel
+from utils import create_connection
+from obtain_features import extract_features
+
+import tensorflow as tf
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--db-path", type=str, default="./rides.db",
+                    help="Path to the sqlite3 database file.")
+
+
+
+def train_on_batches(model, data_generator, data_gen_args):
+
+	model_dir = os.path.join("models",  datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S'))
+	os.mkdir(model_dir)
+
+	save_every = 5
+	num_epochs = 200
+	optimizer = 'Adam'
+	loss = tf.keras.losses.MSE
+	model.compile(optimizer=optimizer, loss=loss)
+
+	iter_num = 0
+	log_file = open(os.path.join(model_dir, "training_logs.txt"), "w")
+
+	for epoch in range(num_epochs):
+		
+		print(f"\nEpoch {epoch+1} begins")
+		epochLoss = 0.0
+		for features, outputs in data_generator(**data_gen_args):
+			iter_num += 1
+			print(f'Training epoch: {epoch+1}, iteration: {iter_num}')
+			if not isinstance(features,np.ndarray):
+				features = features.toarray()
+			if not isinstance(outputs, np.ndarray):
+				outputs = outputs.toarray()
+			MSEloss = model.train_on_batch(x=features, y=outputs)
+			print(f'loss: {MSEloss}')
+			epochLoss += MSEloss
+			log_file.write(f'{epoch+1}, {iter_num}, {MSEloss}\n')
+
+			if iter_num % save_every == 0:
+				tf.keras.models.save_model(model, os.path.join(model_dir, f'model_{iter_num}'))
+
+		print(f"Total Epoch Loss = {epochLoss}")
+		print("======================================================================\n")
+
+	log_file.close()
+
+
+def train(model, data_generator, data_gen_args):
+	
+	features, values = data_generator(**data_gen_args)
+	model_dir = os.path.join("models",  datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S'))
+	os.mkdir(model_dir)
+
+	optimizer = 'Adam'
+	loss = tf.keras.losses.MSE
+	model.compile(optimizer=optimizer, loss=loss)
+
+	num_epochs = 20
+	model.fit(features, values, epochs=num_epochs)
+
+	tf.keras.models.save_model(model, os.path.join(model_dir, f'model_{num_epochs}'))
+
+
+def main():
+	parsed_args = parser.parse_args()
+
+	model = BoroModel([200,50])
+	conn = create_connection(parsed_args.db_path)
+
+	batch_size = 2048
+	block_size = 512
+	variant = 'all'
+
+	data_generator = extract_features
+	data_generator_arguments = {
+			"conn": conn,
+			"table_name": "rides",
+			"variant": variant, 
+			"size": batch_size,
+			"block_size": block_size,
+			"datetime_onehot": False,
+			"weekdays_onehot": True,
+			"include_loc_ids": True
+		}
+
+	if variant == 'all':
+		train(model, data_generator, data_generator_arguments)
+	elif variant == 'batch':
+		train_on_batches(model, data_generator, data_generator_arguments)
+
+
+
+
+if __name__ == "__main__":
+	main()
